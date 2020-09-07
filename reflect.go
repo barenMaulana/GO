2547886e2151ba4e9ba358b33f7f9b8a8c37c1de
BarenMaulana <barenmaulana@gmail.com>
2020-09-07 11:52:06 +0700

    menambahkan fungsi cek tipe data pada file reflect.go

---
+++ reflect.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func checkTipeData(data interface{}){
+	check := reflect.TypeOf(data)
+	fmt.Println(check)
+}
+
+func main(){
+	nama := "baren maulana"
+	umur := 17
+	reflectValue := reflect.ValueOf(umur)
+
+	fmt.Println("tipe data pada variable umur :", reflectValue.Type())
+	fmt.Println("value data pada variable umur :", reflectValue.Interface())
+
+	checkTipeData(umur)
+	checkTipeData(nama)
+	tipe data pada variable umur : int
+}
