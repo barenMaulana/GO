3d3defc68604ee93040dbbf610ad7f222a679938
BarenMaulana <barenmaulana@gmail.com>
2020-09-22 21:31:47 +0700

    menambahkan file closure-function.go

--- pointer.go
+++ pointer.go
@@ -2,13 +2,24 @@ package main
 
 import "fmt"
 
-func change(original *int, value int){
-	 *original = value
+type Student struct{
+	Name,Major string
+	Grade uint8
 }
 
 func main()  {
-	a := 4
+	var siswa1 = Student{"baren maulana","rekayasa perangkat lunak",12}
+	var siswa2 *Student = &siswa1
+	var siswa3 *Student = siswa2
 
-	change(&a, 10)
-	fmt.Println(a)
+	fmt.Println("siswa 1",siswa1)
+	fmt.Println("siswa 2",siswa2)
+	fmt.Println("siswa 3",siswa3)
+
+	fmt.Println("")
+	siswa3.Name = "gatot kaca"
+
+	fmt.Println("siswa 1",siswa1)
+	fmt.Println("siswa 2",siswa2)
+	fmt.Println("siswa 3",siswa3)
 }
